106463d729e6f6fdc50a6f395ee654f323bcb629
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2026-07-04 10:32:30 +0530

    added inngest function

---
+++ backend/src/libs/inngest.js
@@ -0,0 +1,41 @@
+import { inngest } from "inngest";
+import { User } from "../models/User.model.js"
+import { connectDb } from "../db/connectToDB.js";
+
+export const inngest = new Inngest({ id: skillQ });
+
+const syncUser = inngest.createFunction({
+    id: "sync-user"}, { event: "clerk/user.created"},
+     async ({event}) => {
+        await connectDb();
+
+        const {id, image_url, first_name, last_name, email_addresses} = event.data;
+
+        const newUser = {
+            clerkId: id,
+            email: email_addresses[0].email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profilepic: image_url
+        }
+
+        await User.create(newUser);
+     }
+)
+
+const deleteUserFromDB = inngest.createFunction({
+    id: "delete-user"}, { event: "clerk/user.deleted"},
+     async ({event}) => {
+        await connectDb();
+
+        const {id} = event.data;
+
+        await User.deleteOne({ clerkId: id});
+     }
+)
+
+
+
+export const functions = [
+    syncUser,
+    deleteUserFromDB
+]
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import { connectDb } from "./db/connectToDB.js";
 import { ENV } from "./libs/env.js";
+import serve from "inngest/express";
+import { Inngest } from "inngest";
+import { functions, inngest } from "./libs/inngest.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors({
     origin: ENV.ORIGIN_URL, Credentials: true
-}))
+}));
+
+app.use("/api/serve", serve({client: inngest, functions}))
+
 app.get("/", (req, res) => {
     res.send("Helllo this is my new web app!")
 })
