5b75baa875574cbc130a1aa90cf408f984667346
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2026-07-04 16:17:14 +0530

    fixed bugs

--- backend/src/libs/stream.js
+++ backend/src/libs/stream.js
@@ -7,19 +7,19 @@ const apiSecret = ENV.STREAM_SECRET_KEY;
 export const chatClient = StreamChat.getInstance(apiKey, apiSecret);
 
 export const upserStreamtUser = async (userData) => {
-    const upsertNewUser = await chatClient.upsertUser(userData);
-    console.log("Stream user upserted successfully", userData);
+  const upsertNewUser = await chatClient.upsertUser(userData);
+  console.log("Stream user upserted successfully", userData);
 
-    if (!upsertNewUser) {
-        console.log("user was not upserted!");
-    }
+  if (!upsertNewUser) {
+    console.log("user was not upserted!");
+  }
+};
 
-    export const deleteStreamtUser = async (userId) => {
-        const deleteStreamUser = await chatClient.deleteUser(userId);
-        console.log("Stream user was deleted successfully", deleteStreamUser);
+export const deleteStreamtUser = async (userId) => {
+  const deleteStreamUser = await chatClient.deleteUser(userId);
+  console.log("Stream user was deleted successfully", deleteStreamUser);
 
-        if (!deleteStreamUser) {
-            console.log("user was not deleted!");
-        }
-
-    }
+  if (!deleteStreamUser) {
+    console.log("user was not deleted!");
+  }
+};
