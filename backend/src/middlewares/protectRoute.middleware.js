703c9ed982e23fbf05290185537d372762fdfa88
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2026-07-13 22:04:03 +0530

    added protectRoute

--- backend/src/middlewares/protectRoute.middleware.js
+++ backend/src/middlewares/protectRoute.middleware.js
@@ -1,8 +1,30 @@
 import { getAuth } from '@clerk/express';
 import { User } from "../models/User.model.js";
+import { ApiErrors } from "../utils/ApiErrors.js";
 
 
 export const protectRoute = [
     getAuth(),
+    async(req, res, next) => {
+       try {
+         const clerkId = req.auth().userId;
+         
+          if (!clerkId) {
+            throw new ApiErrors(401, "Unauthorized request");
+          }
+ 
+          const user = await User.findOne({clerkId});
+ 
+          if(!user){
+             throw new ApiErrors(404, "user not found");
+          }
+ 
+          req.user = user;
+ 
+          next();
+       } catch (error) {
+        throw new ApiErrors(500, "Internal Server Error");
+       }
+    }
     
 ]
