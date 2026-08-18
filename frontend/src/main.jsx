b4a91859de34c473c651c15525af6f43dc549cec
AKashTiwariOfficial <akashtiwari00624@gmail.com>
2026-08-18 16:27:03 +0530

    Fixed clerk errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,12 +7,17 @@ import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY");
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ClerkProvider>
+        <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
           <App />
         </ClerkProvider>
       </QueryClientProvider>
